8b31746f012798779c34714efe6e5f3eddac28d8
Ahlie Santiago <83854486+ahliesantiago@users.noreply.github.com>
2025-07-30 20:49:40 +0800

    [feat]: add doctor and appointment info to overview

--- src/main/java/com/hospitalname/service/OverallService.java
+++ src/main/java/com/hospitalname/service/OverallService.java
@@ -1,13 +1,17 @@
 package com.hospitalname.service;
 import com.hospitalname.service.department.DepartmentService;
+import com.hospitalname.service.doctor.DoctorQueryHandler;
 import com.hospitalname.service.patient.PatientQueryHandler;
 
 public class OverallService {
     DepartmentService departmentService = new DepartmentService();
+    DoctorQueryHandler doctorQuery = new DoctorQueryHandler();
     PatientQueryHandler patientQuery = new PatientQueryHandler();
 
     public void getOverallInfo() {
         System.out.println("Departments: " + departmentService.countDepartments());
-        System.out.println("Patients: " + patientQuery.countPatients());
+        System.out.println("Doctors: " + doctorQuery.countRecords());
+        System.out.println("Patients: " + patientQuery.countRecords());
+        System.out.println("Upcoming Appointments: " + 0);
     }
 }
